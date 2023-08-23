a2425992dcb6377bbde2fc8c1471496833ebdb16
mohsin094 <mohsinkhanzda@gmail.com>
2023-08-23 11:00:01 +0500

    Setup

---
+++ database/migrations/2023_08_23_044354_create_feedback_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('order_id')->nullable();
+            $table->string('day_in_use');
+            $table->tinyInteger('rating')->unsigned();
+            $table->string('comment')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedback');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/admin', [App\Http\Controllers\DashboardController::class, 'index'])->name('admin');
